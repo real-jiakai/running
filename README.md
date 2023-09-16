fd3ad319c0949166aaf0a51e9947d51f1be8a8ae
jiakai <gujiakai28@gmail.com>
2023-09-16 09:31:27 +0800

    feat: initial comit

---
+++ README.md
@@ -0,0 +1,5 @@
+![Vercel](https://vercelbadge.vercel.app/api/real-jiakai/running)
+
+# Introduction
+
+The repo stores my running site, based on [running_page](https://github.com/yihong0618/running_page).
---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: 'Running Page',
+    siteUrl: 'https://run.gujiakai.top',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    description: 'Running Page',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://blog.gujiakai.top',
+      },
+      {
+        name: 'About',
+        url: 'https://blog.gujiakai.top/about',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+
+    {
+      resolve: 'gatsby-plugin-vercel',
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: '.',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
+};
